f3b340304d156f08189949fde31e1930128b8376
ZhiChen <karan.shaw@outlook.com>
2017-07-22 23:50:58 +0800

    Add Module Footer

---
+++ src/components/common/footer/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <Footer class="clearfix">
+    <img src="http://via.placeholder.com/140x140">
+    <div class="contact">
+        <p>全国咨询热线：400-805-8023</p>
+        <p>办公地址：上海市杨浦区复旦大学</p>
+        <p>沪ICP备16015612号-2</p>
+        <p>邮箱：service@bangnongmang.cn</p>
+    </div>
+  </Footer>
+</template>
+
+<script>
+export default {
+  name: 'zfooter'
+}
+</script>
+
+
+<style lang="scss" scoped>
+Footer {
+    background-color: #282828;
+    padding: 2rem 0;
+    
+
+    img {
+        float: left;
+        margin-left: 23rem;
+    }
+}
+
+.contact {
+    color: #fff;
+    font-size: 1rem;
+    float: left;
+    margin-left: 2.25rem;
+    height: 8.75rem;
+    p {
+        margin-bottom: .875rem;
+    }
+}
+</style>
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
       <z-header></z-header>
+      <z-footer></z-footer>
   </div>
 </template>
 
 <script>
 import zheader from './../common/header/Header';
+import zfooter from './../common/footer/Footer';
 
 export default {
   name: 'home',
   components: {
-      'z-header': zheader
+      'z-header': zheader,
+      'z-footer': zfooter
   }
 }
 </script>
